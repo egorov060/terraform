b42ec83ee424dc0692aba4ce218623fd99a07cdb
Egorov <egor35@yandex.ru>
2022-08-31 18:54:49 +0200

    test

---
+++ config.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "y0_AgAAAABjv7fsAATuwQAAAADNd1PsLO8otNcKSu-8ypOLZddyTkPTeQs"
+  cloud_id  = "b1g9q49j8o9p8fdicr7g"
+  folder_id = "b1gphfspgsih2mk1gt8o"
+  zone      = "ru-central1-a"
+}
+
+
+#####
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "build"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81hgrcv6lsnkremf32"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("/home/vm1/terraform1/meta_build.txt")}"
+  }
+
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "prod"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81hgrcv6lsnkremf32"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("/home/vm1/terraform1/meta_prod.txt")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
